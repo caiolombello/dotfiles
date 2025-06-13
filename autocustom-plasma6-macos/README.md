5ab377a19f83f35f128de6a4d8a198f388b76fb2
Caio Barbieri <caio@lombello.com>
2025-06-12 23:21:43 -0300

    Add installation scripts and configuration files for KDE Plasma 6 customization to resemble macOS. Includes support for Fedora, KDE Neon, Manjaro, and openSUSE, along with various assets like themes, icons, and fonts.

---
+++ autocustom-plasma6-macos/README.md
@@ -0,0 +1,78 @@
+Turn your KDE Plasma 6 desktop into a stunning macOS-inspired interface with our easy-to-use script! Whether you’re using Fedora 40 KDE Plasma, KDE Neon, Manjaro KDE Plasma, or openSUSE Tumbleweed, this tool will automatically handle everything you need to transform your system into a beautiful, macOS-like experience.
+
+### Supported Systems:
+
+- **Fedora 40 KDE Plasma**
+- **KDE Neon**
+- **Manjaro Linux KDE Plasma**
+- **openSUSE Tumbleweed**
+
+### What You’ll Get:
+
+- **A Complete macOS Makeover**: Our script does all the heavy lifting—installing a gorgeous Plasma theme, icons, fonts, widgets, Latte Dock setup, wallpapers, and a cursor theme. Plus, it tweaks your system settings to give you that seamless macOS vibe.
+    
+- **Smooth and Polished**: Not only will your desktop look like macOS, but it will also feel super smooth with crisp animations and intuitive layouts that make everyday tasks a breeze.
+    
+- **One-Click Revert**: If you ever want to switch back to the default KDE look, no worries! We’ve included easy-to-use uninstall scripts that will quickly return your desktop to its original setup.
+    
+### Included Files:
+
+**Install Scripts**
+
+- `install_fedora.sh` (for Fedora 40 KDE Plasma)
+- `install_kdeneon.sh` (for KDE Neon)
+- `install_manjaro.sh` (for Manjaro Linux KDE Plasma)
+- `install_opensuse.sh` (for openSUSE Tumbleweed KDE Plasma)
+
+### How to Use:
+
+1. **Extract the Package**:
+    
+    ```bash
+    unzip autocustom-plasma6-macos-v1.0.zip
+    
+    ```
+    
+2. **Go to the Directory**:
+    
+    ```bash
+    cd autocustom-plasma6-macos-v1.0/
+    
+    ```
+    
+3. **Run the Installation Script** for your system:
+    
+    - **Fedora 40 KDE Plasma**:
+        
+        ```bash
+        chmod +x install_fedora.sh
+        ./install_fedora.sh
+        
+        ```
+        
+    - **KDE Neon**:
+        
+        ```bash
+        chmod +x install_kdeneon.sh
+        ./install_kdeneon.sh
+        
+        ```
+        
+    - **Manjaro Linux KDE Plasma**:
+        
+        ```bash
+        chmod +x install_manjaro.sh
+        ./install_manjaro.sh
+        
+        ```
+        
+    - **openSUSE Tumbleweed**:
+        
+        ```bash
+        chmod +x install_opensuse.sh
+        ./install_opensuse.sh
+        
+        ```
+        
+
+And that’s it! With just a few simple steps, you’ll be enjoying a sleek and elegant macOS-like desktop on your KDE Plasma 6 system. Ready to give your setup a fresh look? Dive in and start customizing!
---
+++ autocustom-plasma6-macos/install_opensuse.sh
@@ -0,0 +1,314 @@
+#!/bin/bash
+#
+# Script Name: install_manjaro.sh
+# Version : 2.0
+# Description: Installation script auto customization
+# KDE Plasma 6 Look Like macOS layout
+#
+# For Linux : openSUSE Tumbleweed
+# For Desktop Environment : KDE Plasma 6
+# Author: linuxscoop
+# Youtube: https://youtube.com/user/linuxscoop
+# Created: Nov 21, 2024
+# Last Modified: Nov 25, 2024
+# Function to prompt for sudo password and exit if incorrect
+PLASMA6MACOS_check_sudo() {
+  if ! sudo -v; then
+    echo "Incorrect sudo password. Exiting."
+    exit 1
+  fi
+
+  # Keep sudo alive until the script finishes
+  while true; do
+    sudo -n true
+    sleep 60
+    kill -0 "$$" || exit
+  done 2>/dev/null &
+}
+# Function to update the system
+PLASMA6MACOS_update_system() {
+    sudo zypper refresh && sudo zypper dup -y
+}
+
+# Function to install command line apps and dependencies
+PLASMA6MACOS_install_dependencies() {
+    sudo zypper install -y libqt5-qttools curl wget rsync git nautilus gnome-terminal nautilus-extension-terminal sassc kdeconnect-kde
+}
+
+# Function to install GNOME apps
+PLASMA6MACOS_install_gnome_apps() {
+    sudo zypper install -y gnome-weather gnome-maps gnome-calendar gnome-clocks vlc
+}
+# Function to install the Zsh shell
+PLASMA6MACOS_install_zsh_shell() {
+    sudo zypper install -y zsh
+    sudo chsh -s $(which zsh)
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" <<EOF
+Y
+EOF
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh/custom/plugins/zsh-history-substring-search
+}
+
+# Function to install Oh My Posh
+PLASMA6MACOS_install_ohmyposh() {
+    # Define variables
+    OMP_BINARY="/usr/local/bin/oh-my-posh"
+    OMP_DOWNLOAD_URL="https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64"
+
+    # Download Oh My Posh binary if it doesn't already exist
+    if [ -f "$OMP_BINARY" ]; then
+        echo "Oh My Posh is already installed. Skipping download."
+    else
+        if sudo wget "$OMP_DOWNLOAD_URL" -O "$OMP_BINARY"; then
+            echo "Downloaded Oh My Posh binary successfully."
+        else
+            echo "Failed to download Oh My Posh binary."
+            exit 1
+        fi
+    fi
+    # Set execute permissions on the binary
+    sudo chmod +x "$OMP_BINARY"
+    # Unzip configuration files
+    if unzip -o assets/plasma6macos-zshomh-config.zip -d "$HOME"; then
+        echo "Fish Shell and Oh My Posh config installed successfully."
+    else
+        echo "Failed to unzip Fish Shell and Oh My Posh."
+        exit 1
+    fi
+
+    # Set permissions for themes
+    if chmod u+rw "$HOME/.poshthemes/"*.json; then
+        echo "Set read and write permissions for Oh My Posh themes."
+    else
+        echo "Failed to set permissions for Oh My Posh themes."
+    fi
+
+    echo "Oh My Posh installation completed."
+}
+
+# Function to enable Flatpak, AppImage support, and add Flathub repository
+PLASMA6MACOS_enable_flatpak_appimage() {
+    sudo zypper install -y flatpak fuse
+    sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+}
+
+# Install Flatpak applications
+PLASMA6MACOS_install_flatpak_apps() {
+    sudo flatpak install -y flathub io.bassi.Amberol #com.mattjakeman.ExtensionManager com.github.KRTirtho.Spotube
+    OVERRIDE_FILE="$HOME/.local/share/flatpak/overrides/global"
+
+    # Ensure the overrides directory exists
+    mkdir -p "$(dirname "$OVERRIDE_FILE")"
+
+    # Write the new content to the global file, replacing any existing content
+    cat <<EOL > "$OVERRIDE_FILE"
+    [Context]
+    filesystems=~/.local/share/icons;~/.themes;xdg-config/gtk-4.0;xdg-config/gtk-3.0
+EOL
+
+    echo "Flatpak overrides have been set in $OVERRIDE_FILE"
+}
+
+PLASMA6MACOS_install_plasma_theme() {
+    unzip -o assets/plasma6macos-plasma-theme.zip -d $HOME
+}
+
+# Function to install Kvantum Manager
+PLASMA6MACOS_install_kvantum_manager() {
+    sudo zypper install -y kvantum-manager
+    unzip -o assets/plasma6macos-kvantum-config.zip -d $HOME
+}
+
+PLASMA6MACOS_install_gtk_theme() {
+    unzip -o assets/plasma6macos-gtk-theme-config.zip -d $HOME
+}
+
+PLASMA6MACOS_install_icon_theme() {
+    unzip -o assets/plasma6macos-icon-packs.zip -d $HOME
+}
+
+PLASMA6MACOS_install_cursors_theme() {
+    unzip -o assets/plasma6macos-cursors.zip -d $HOME
+}
+
+PLASMA6MACOS_install_fonts() {
+    unzip -o assets/plasma6macos-font-packs.zip -d $HOME
+}
+
+PLASMA6MACOS_install_wallpapers() {
+    unzip -o assets/plasma6macos-wallpaper-packs.zip -d $HOME
+}
+
+PLASMA6MACOS_install_konsole_profile() {
+    unzip -o assets/plasma6macos-konsole-profile.zip -d $HOME
+}
+
+PLASMA6MACOS_install_plasmoids_widget() {
+    unzip -o assets/plasma6macos-plasmoids-widget.zip -d $HOME
+}
+
+# Function to install the Plymouth macOS theme
+PLASMA6MACOS_install_plymouth() {
+    sudo unzip -o assets/plasma6macos-plymouth-config.zip -d /
+    sudo plymouth-set-default-theme -R macOS
+}
+# Function to install Albert
+PLASMA6MACOS_install_albert() {
+    sudo zypper --gpg-auto-import-keys addrepo https://download.opensuse.org/repositories/home:manuelschneid3r/openSUSE_Tumbleweed/home:manuelschneid3r.repo    sudo zypper refresh
+    sudo zypper --gpg-auto-import-keys refresh
+    sudo zypper install -y albert
+    unzip -o assets/plasma6macos-albertlauncher-config.zip -d $HOME
+    sudo unzip -o assets/plasma6macos-albertlauncher-theme.zip -d /
+}
+
+# Function to install the SDDM macOS Sequoia theme
+PLASMA6MACOS_install_sddm_theme() {
+    sudo unzip -o assets/plasma6macos-sddm-config.zip -d /
+}
+
+# Function to install Plasma 6 macOS resources
+PLASMA6MACOS_install_plasma6macos_config() {
+    kquitapp6 plasmashell
+    unzip -o assets/plasma6macos-kde-config.zip -d $HOME
+    kstart plasmashell &> /dev/null &
+    dconf load / < assets/plasma6macos-gnome-config.conf
+    #Enable gtk theme dark mode
+    ln -sf "$HOME/.config/gtk-4.0/gtk-Dark.css" "$HOME/.config/gtk-4.0/gtk.css"
+}
+
+# Function to install Firefox Safari theme
+PLASMA6MACOS_install_firefox_safari_theme() {
+# Create and enter src directory
+    [ ! -d "src" ] && mkdir src
+    cd src || exit
+
+    # Check if WhiteSur-gtk-theme exists
+    if [ ! -d "WhiteSur-gtk-theme" ]; then
+        git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
+    fi
+
+    # Enter the directory and run the tweaks script
+    cd WhiteSur-gtk-theme || exit
+    ./tweaks.sh -f monterey 5+5
+
+    # Return to previous directory
+    cd ../..
+}
+
+# Function to install KWin effects force blur
+PLASMA6MACOS_install_kwin_effect() {
+    unzip -o assets/plasma6macos-kwin-effect.zip -d $HOME
+    sudo zypper in -y git cmake-full gcc-c++ kf6-extra-cmake-modules kcoreaddons-devel kguiaddons-devel kconfigwidgets-devel kwindowsystem-devel ki18n-devel kiconthemes-devel kpackage-devel frameworkintegration-devel kcmutils-devel kirigami2-devel "cmake(KF6Config)" "cmake(KF6CoreAddons)" "cmake(KF6FrameworkIntegration)" "cmake(KF6GuiAddons)" "cmake(KF6I18n)" "cmake(KF6KCMUtils)" "cmake(KF6KirigamiPlatform)" "cmake(KF6WindowSystem)" "cmake(Qt6Core)" "cmake(Qt6DBus)" "cmake(Qt6Quick)" "cmake(Qt6Svg)" "cmake(Qt6Widgets)" "cmake(Qt6Xml)" "cmake(Qt6UiTools)" "cmake(KF6Crash)" "cmake(KF6GlobalAccel)" "cmake(KF6KIO)" "cmake(KF6Service)" "cmake(KF6Notifications)" libepoxy-devel kwin6-devel
+    # Create and enter src directory
+    [ ! -d "src" ] && mkdir src
+    cd src || exit
+
+    # Clone and install kwin-effects-forceblur
+    if [ ! -d "kwin-effects-forceblur" ]; then
+        git clone https://github.com/taj-ny/kwin-effects-forceblur.git
+    fi
+
+    cd kwin-effects-forceblur || exit
+    mkdir -p build
+    cd build || exit
+    cmake ../ -DCMAKE_INSTALL_PREFIX=/usr
+    make
+    sudo make install
+
+    # Return to previous directory
+    cd ../../..
+}
+# Function to prompt for logout after setup completion
+PLASMA6MACOS_prompt_for_logout() {
+    clear
+    read -p "Setup completed successfully. Please logout and log back in for changes to take effect. (y/n): " choice
+    if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
+        qdbus6 org.kde.Shutdown /Shutdown org.kde.Shutdown.logout
+    else
+        clear
+        echo "You can manually logout and log back in later."
+    fi
+}
+
+# Start the script execution
+clear
+echo "===================================================================="
+echo " Auto Customize KDE Plasma 6 Look Like macoS on openSUSE Tumbleweed "
+echo "===================================================================="
+# Detailed prompt for user confirmation
+echo
+read -p "Would you like to proceed with the customization? (yes/no): " response
+
+# Check the response
+if [[ "$response" =~ ^(yes|y|Y)$ ]]; then
+    echo "Great choice! Starting the customization process... 🎉"
+    PLASMA6MACOS_check_sudo
+    clear
+    echo "Updating your system with new packages..."
+    PLASMA6MACOS_update_system
+    clear
+    echo "Installing Dependencies..."
+    PLASMA6MACOS_install_dependencies
+    clear
+    echo "Installing GNOME Apps..."
+    PLASMA6MACOS_install_gnome_apps
+    clear
+    echo "Installing Plasma 6 macOS Theme..."
+    PLASMA6MACOS_install_plasma_theme
+    clear
+    echo "Installing Kvantum Manager and Theme..."
+    PLASMA6MACOS_install_kvantum_manager
+    clear
+    echo "Installing GTK macOS Theme..."
+    PLASMA6MACOS_install_gtk_theme
+    clear
+    echo "Installing Icon Theme..."
+    PLASMA6MACOS_install_icon_theme
+    clear
+    echo "Installing Cursors Theme..."
+    PLASMA6MACOS_install_cursors_theme
+    clear
+    echo "Installing Fonts and Wallpapers..."
+    PLASMA6MACOS_install_fonts
+    PLASMA6MACOS_install_wallpapers
+    clear
+    echo "Enable Flatpak and AppImage Support..."
+    PLASMA6MACOS_enable_flatpak_appimage
+    clear
+    echo "Installing Flatpak Apps..."
+    PLASMA6MACOS_install_flatpak_apps
+    clear
+    echo "Installing ZSH Shell + OhMyPOSH..."
+    PLASMA6MACOS_install_zsh_shell
+    PLASMA6MACOS_install_ohmyposh
+    clear
+    echo "Installing Konsole terminal profile..."
+    PLASMA6MACOS_install_konsole_profile
+    clear
+    echo "Installing SDDM theme..."
+    PLASMA6MACOS_install_sddm_theme
+    clear
+    echo "Installing PlyMouth Theme..."
+    PLASMA6MACOS_install_plymouth
+    clear
+    echo "Installing Albert Launcher..."
+    PLASMA6MACOS_install_albert
+    clear
+    echo "Installing Plasmoids Widgets..."
+    PLASMA6MACOS_install_plasmoids_widget
+    clear
+    echo "Installing KWin Effect..."
+    PLASMA6MACOS_install_kwin_effect
+    clear
+    echo "Installing Safari Theme for Firefox..."
+    PLASMA6MACOS_install_firefox_safari_theme
+    clear
+    echo "Installing Plasma 6 macoS Configurations..."
+    PLASMA6MACOS_install_plasma6macos_config
+    clear
+    PLASMA6MACOS_prompt_for_logout
+    else
+        echo "Got it! If you change your mind, just run this script again. 😊"
+fi
